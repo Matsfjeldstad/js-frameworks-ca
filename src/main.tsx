5c7c059eb944e6ba45ac1cd967bd975492daf543
Mats Fjeldstad <mats.fjeldstad@outlook.com>
2023-03-26 21:06:57 +0200

    adds layout.jsx with gloabl header and footer

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import Layout from './Layout';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Layout />
     </BrowserRouter>
   </React.StrictMode>,
 );
