f101bf5cc89a2420633855caee8bd4da7f7e7388
Mats Fjeldstad <mats.fjeldstad@outlook.com>
2023-04-09 22:18:23 +0200

    feat(Header.tsx): add search bar modal functionality feat(MappedProducts.tsx): add MappedProducts component to display products
    feat(SearchBar.tsx): add SearchBar component
    refactor(HomePage.tsx): move MappedProducts to singleComponents folder

--- src/components/singleComponents/MappedProducts.tsx
+++ src/components/singleComponents/MappedProducts.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { useGetProductsQuery } from './../store/modules/apiSlice';
+import { useGetProductsQuery } from '../../store/modules/apiSlice';
 
 const MappedProducts = () => {
   const { data: products, error, isLoading } = useGetProductsQuery();
--- src/components/views/HomePage.tsx
+++ src/components/views/HomePage.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-import MappedProducts from '../MappedProducts';
+import MappedProducts from '../singleComponents/MappedProducts';
 
 export default function HomePage() {
   return (
-    <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+    <div className="mx-auto max-w-2xl py-16 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
       <h2 className="text-2xl font-bold tracking-tight text-gray-900">Products</h2>
       <MappedProducts></MappedProducts>
     </div>
