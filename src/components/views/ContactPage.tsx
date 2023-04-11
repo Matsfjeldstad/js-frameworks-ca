c7010c9e6268d713ec31b66802cc377db2756a57
Mats Fjeldstad <mats.fjeldstad@outlook.com>
2023-04-11 17:06:38 +0200

    feat(ContactPage.tsx): add form validation and error messages feat(ContactPage.tsx): add form submission alert feat(ContactPage.tsx): add animation to form labels feat(ContactPage.tsx): disable submit button until form is valid and dirty fix(ProductCart.tsx): fix calculation of total price with shipping cost

--- src/components/views/ContactPage.tsx
+++ src/components/views/ContactPage.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import noisy_gradient from '/noisy_gradient.jpg';
+import { useAutoAnimate } from '@formkit/auto-animate/react';
 
 type Props = {};
 
 export default function ContactPage({}: Props) {
+  const [animationParent] = useAutoAnimate();
+
   const formik = useFormik({
     initialValues: {
       name: '',
@@ -15,14 +18,18 @@ export default function ContactPage({}: Props) {
     },
 
     validationSchema: Yup.object({
+      name: Yup.string().required('Full Name is Required').min(3, 'Minimum number of characters is 3'),
       email: Yup.string().email('Email is not valid').required('Email is Required'),
+      subject: Yup.string().required('Subject is Required').min(3, 'Minimum number of characters is 3'),
+      message: Yup.string().required('Message is Required').min(3, 'Minimum number of characters is 3'),
     }),
     onSubmit: (values) => {
-      console.log(values);
+      alert(`Name: ${values.name} Email: ${values.email} Subject: ${values.subject} Message: ${values.message}`);
     },
   });
 
-  console.log(formik.errors);
+  const errorClasses = 'border-2 border-red-600';
+
   return (
     <main className="flex flex-col bg-gray-200 p-4">
       <div className="text-center mt-9">
@@ -48,45 +55,78 @@ export default function ContactPage({}: Props) {
         </div>
         <div className="w-full md:w-3/5">
           <form onSubmit={formik.handleSubmit} action="" className=" flex flex-col gap-4 max-w-3xl p-6">
-            <label className="flex flex-col gap-1" htmlFor="name">
+            <label ref={animationParent} className="flex flex-col gap-1" htmlFor="name">
               Full Name
+              {formik.touched && formik.errors.name && (
+                <div className="text-red-800 font-bold">{formik.errors.name}</div>
+              )}
               <input
-                className="p-4 border border-gray-400 bg-white rounded-sm"
+                className={`p-4 border border-gray-400 bg-white rounded-sm ${
+                  formik.touched && formik.errors.name && errorClasses
+                }`}
                 type="text"
                 id="name"
                 placeholder="Full Name"
-                onChange={formik.handleChange}
-              />
-            </label>
-            <label className="flex flex-col gap-1" htmlFor="email">
-              Email
-              <input
-                className="p-4 border border-gray-400 bg-white rounded-sm"
-                type="email"
-                id="email"
-                placeholder="Your email"
+                value={formik.values.name}
                 onChange={formik.handleChange}
                 onBlur={formik.handleBlur}
               />
             </label>
-            <label className="flex flex-col gap-1" htmlFor="subject">
-              Subject
+            <label ref={animationParent} className="flex flex-col gap-1" htmlFor="email">
+              Email
+              {formik.touched && formik.errors.email && (
+                <div className="text-red-800 font-bold">{formik.errors.email}</div>
+              )}
               <input
-                className="p-4 border border-gray-400 bg-white rounded-sm"
+                className={`p-4 border border-gray-400 bg-white rounded-sm ${
+                  formik.touched && formik.errors.email && errorClasses
+                }`}
+                type="email"
+                id="email"
+                placeholder="Your email"
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+              />
+            </label>
+            <label ref={animationParent} className="flex flex-col gap-1" htmlFor="subject">
+              Subject
+              {formik.touched && formik.errors.subject && (
+                <div className="text-red-800 font-bold">{formik.errors.subject}</div>
+              )}
+              <input
+                className={`p-4 border border-gray-400 bg-white rounded-sm ${
+                  formik.touched && formik.errors.subject && errorClasses
+                }`}
                 type="text"
                 id="subject"
                 placeholder="Subject"
+                value={formik.values.subject}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
               />
             </label>
-            <label className="flex flex-col gap-1" htmlFor="messageInput">
+            <label ref={animationParent} className="flex flex-col gap-1" htmlFor="messageInput">
               Message
+              {formik.touched && formik.errors.message && (
+                <div className="text-red-800 font-bold">{formik.errors.message}</div>
+              )}
               <textarea
-                className="p-4 rounded-sm min-h-[140px] border border-gray-400"
+                className={`p-4 border border-gray-400 bg-white rounded-sm ${
+                  formik.touched && formik.errors.message && errorClasses
+                }`}
                 id="message"
                 placeholder="Your message"
+                value={formik.values.message}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
               />
             </label>
-            <button type="submit" className="p-2 text-white bg-blue-600 rounded-lg">
+            <button
+              disabled={!(formik.isValid && formik.dirty)}
+              type="submit"
+              className="p-2 text-white bg-blue-600 rounded-lg disabled:bg-slate-400"
+            >
               Submit
             </button>
           </form>
